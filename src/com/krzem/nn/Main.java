e08ee3ddb1fe165d1532f0e37847ee6fdec6d0ca
Krzem <krzem5.dev@gmail.com>
2021-04-11 10:23:56 +0200

    Push Update 04/11/2021, 10:23:50

--- src/com/krzem/nn/Main.java
+++ src/com/krzem/nn/Main.java
@@ -1,10 +1,11 @@
-package com.krzem.NN;
+package com.krzem.nn;
 
 
 
 public class Main{
 	public static void main(String[] args){
-		NeuralNetwork nn=new NeuralNetwork(2,1,0.01);
+		int[] h={4};
+		NeuralNetwork nn=new NeuralNetwork(2,h,1,0.01);
 		// NeuralNetwork nn=NeuralNetwork.fromFile("./data.nn-data");
 		double[][][] D={{{0,0},{0}},{{0,1},{1}},{{1,0},{1}},{{1,1},{0}}};
 		nn.train_multiple(D,D,200000,0,100,true);
